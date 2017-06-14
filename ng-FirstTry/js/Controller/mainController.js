96929a4160dc2f9cb3d2dbe4a67f65907b5947ac
SatyaMutyala <satyanarayana.mutyala@hotmail.com>
2017-06-14 18:45:39 -0400

    interval and log services

--- ng-FirstTry/js/Controller/mainController.js
+++ ng-FirstTry/js/Controller/mainController.js
@@ -2,7 +2,7 @@
 
 var myApp = angular.module("myApp",[]);
 
-var MainController = function($scope, $http){
+var MainController = function($scope, $http, $interval, $log){
 
     var onUserComplete = function(response){
         $scope.user = response.data;
@@ -18,15 +18,37 @@ var MainController = function($scope, $http){
         $scope.error = "Could not fetch data";
     }
 
+    var decrementCountdown= function()
+    {
+        $scope.countdown -= 1;
+        if($scope.countdown < 1)
+        {
+            $scope.search($scope.username);
+        }
+    }
+    var countdownInterval = null;
+    var startCountdown = function()
+    {
+        countdownInterval = $interval(decrementCountdown, 1000, $scope.countdown);
+    }
+
     $scope.search = function(username){
+        $log.info("Searching for user " + username)
         $http.get("https://api.github.com/users/" + username)
          .then(onUserComplete, onError);
+
+        if(countdownInterval){
+            $interval.cancel(countdownInterval);
+            $scope.countdown = null;
+        }
     }
       
     $scope.message = "Hello Angular!";    
     $scope.username = "Angular";
     $scope.repoSortOrder = "-stargazers_count";
+    $scope.countdown = 5;
+    startCountdown();
 }
-myApp.controller("MainController", ["$scope","$http", MainController]);
+myApp.controller("MainController", MainController);
 
 }());
