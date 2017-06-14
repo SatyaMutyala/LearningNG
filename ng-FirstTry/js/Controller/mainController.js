46117619fa91c2f4ee2daa64f627d0a0b639fd1d
SatyaMutyala <satyanarayana.mutyala@hotmail.com>
2017-06-14 16:20:36 -0400

    hard coded changes for Sort filter

--- ng-FirstTry/js/Controller/mainController.js
+++ ng-FirstTry/js/Controller/mainController.js
@@ -6,10 +6,16 @@ var MainController = function($scope, $http){
 
     var onUserComplete = function(response){
         $scope.user = response.data;
+        $http.get($scope.user.repos_url)
+             .then(onRepos, onError);
     }
 
+    var onRepos = function(response){
+        $scope.repos = response.data;
+    }
+    
     var onError = function(reason){
-        $scope.error = "Could not fetch the user";
+        $scope.error = "Could not fetch data";
     }
 
     $scope.search = function(username){
