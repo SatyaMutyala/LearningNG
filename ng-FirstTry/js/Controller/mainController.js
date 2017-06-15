aa041fac78888be8ededa252e7078e90aaf20fac
SatyaMutyala <satyanarayana.mutyala@hotmail.com>
2017-06-15 02:17:18 -0400

    github custom service definition in Angular

--- ng-FirstTry/js/Controller/mainController.js
+++ ng-FirstTry/js/Controller/mainController.js
@@ -2,16 +2,20 @@
 
 var myApp = angular.module("myApp",[]);
 
-var MainController = function($scope, $http, $interval, $log){
+var MainController = function(
+    $scope, github, $interval, 
+    $log, $location, $anchorScroll){
 
-    var onUserComplete = function(response){
-        $scope.user = response.data;
-        $http.get($scope.user.repos_url)
+    var onUserComplete = function(data){
+        $scope.user = data;
+        github.getRepos($scope.user)
              .then(onRepos, onError);
     }
 
-    var onRepos = function(response){
-        $scope.repos = response.data;
+    var onRepos = function(data){
+        $scope.repos = data;
+        $location.hash("userDetails");
+        $anchorScroll();
     }
     
     var onError = function(reason){
@@ -34,8 +38,8 @@ var MainController = function($scope, $http, $interval, $log){
 
     $scope.search = function(username){
         $log.info("Searching for user " + username)
-        $http.get("https://api.github.com/users/" + username)
-         .then(onUserComplete, onError);
+        github.getUser(username)
+              .then(onUserComplete, onError);
 
         if(countdownInterval){
             $interval.cancel(countdownInterval);
